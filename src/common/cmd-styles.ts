5e5ca7b8085c8ec23430677132f4b9aeb2817853
Tyler Durham <TylerDurham@users.noreply.github.com>
2022-05-07 18:04:28 -0500

    Moved style functions to module-level.

--- src/common/cmd-styles.ts
+++ src/common/cmd-styles.ts
@@ -1,9 +1,7 @@
 import chalk from 'chalk';
 
-export const style = {
-    keywords: chalk.magenta,
-    error: chalk.red,
-    warning: chalk.yellow,
-    ok: chalk.green,
-    bold: chalk.bold
-}
+export const keywords = chalk.magenta;
+export const error = chalk.red;
+export const warning = chalk.yellow;
+export const ok = chalk.green;
+export const bold = chalk.bold;
