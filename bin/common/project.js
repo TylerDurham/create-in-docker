918ae3b7176db02878e97fda01dbd06e88c4dd66
Tyler Durham <TylerDurham@users.noreply.github.com>
2022-05-07 18:36:30 -0500

    Implemented npm project setup.

--- bin/common/project.js
+++ bin/common/project.js
@@ -22,19 +22,51 @@ var __importStar = (this && this.__importStar) || function (mod) {
     __setModuleDefault(result, mod);
     return result;
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 exports.__esModule = true;
 exports.ensure = void 0;
 var fs = __importStar(require("fs"));
+var path_1 = __importDefault(require("path"));
+var child_process_1 = require("child_process");
+var cmd_styles_1 = require("./cmd-styles");
+/**
+ * Ensures that the project directory exists and package.json exists.
+ * @param args The current working directory and the project directory name.
+ */
 var ensure = function (args) {
+    ensureDirectory(args);
+    ensurePackageJson(args);
 };
 exports.ensure = ensure;
+/**
+ * Ensures the project directory exists... creates the directory if it doesn't exist.
+ * @param args The current working directory and the project directory name.
+ */
 var ensureDirectory = function (args) {
     try {
         fs.accessSync(args.cwd, fs.constants.R_OK);
+        console.debug("Sweet... project directory ".concat((0, cmd_styles_1.keywords)(args.project), " already exists."));
     }
     catch (err) {
-        console.debug("Creating project directory ".concat(args.project));
+        console.debug("Creating project directory ".concat((0, cmd_styles_1.keywords)(args.project), " because it did not exist."));
         fs.mkdirSync(args.cwd);
         process.chdir(args.cwd);
+    }
+};
+/**
+ * Ensures package.json exists in the current project directory... creates package.json if it doesn't exist.
+ * @param args The current working directory and the project directory name.
+ */
+var ensurePackageJson = function (args) {
+    var fileName = "package.json";
+    try {
+        fs.accessSync(path_1["default"].join(args.cwd, fileName), fs.constants.R_OK);
+        console.debug("Cool! ".concat((0, cmd_styles_1.keywords)(fileName), " already exists in ").concat((0, cmd_styles_1.keywords)(args.project), "."));
+    }
+    catch (_a) {
+        console.debug("Creating ".concat((0, cmd_styles_1.keywords)(fileName), " in ").concat((0, cmd_styles_1.keywords)(args.project), " because it did not exist."));
+        (0, child_process_1.execSync)("npm init -y").toString();
     }
 };
--- src/common/project.ts
+++ src/common/project.ts
@@ -1,17 +1,44 @@
-import { ICmdArgs } from "./interfaces"
 import * as fs from 'fs';
+import path from 'path';
+import { execSync } from 'child_process';
+import { ICmdArgs } from './interfaces';
+import { keywords } from './cmd-styles';
 
+/**
+ * Ensures that the project directory exists and package.json exists.
+ * @param args The current working directory and the project directory name.
+ */
 export const ensure = (args: ICmdArgs) => {
-    
+    ensureDirectory(args);
+    ensurePackageJson(args);
 }
 
-const ensureDirectory  = (args: ICmdArgs) => {
+/**
+ * Ensures the project directory exists... creates the directory if it doesn't exist.
+ * @param args The current working directory and the project directory name.
+ */
+const ensureDirectory = (args: ICmdArgs) => {
     try {
         fs.accessSync(args.cwd, fs.constants.R_OK);
+        console.debug(`Sweet... project directory ${keywords(args.project)} already exists.`);
     } catch (err) {
-        console.debug(`Creating project directory ${args.project}`);
+        console.debug(`Creating project directory ${keywords(args.project)} because it did not exist.`);
         fs.mkdirSync(args.cwd);
         process.chdir(args.cwd);
     }
+}
 
+/**
+ * Ensures package.json exists in the current project directory... creates package.json if it doesn't exist.
+ * @param args The current working directory and the project directory name.
+ */
+const ensurePackageJson = (args: ICmdArgs) => {
+    const fileName = "package.json";
+    try {
+        fs.accessSync(path.join(args.cwd, fileName), fs.constants.R_OK);
+        console.debug(`Cool! ${keywords(fileName)} already exists in ${keywords(args.project)}.`);
+    } catch {
+        console.debug(`Creating ${keywords(fileName)} in ${keywords(args.project)} because it did not exist.`);
+        execSync(`npm init -y`).toString();
+    }
 }
