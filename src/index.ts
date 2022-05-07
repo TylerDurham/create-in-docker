80b1d6158f34567f160754f4dcd6ae04d95214e7
Tyler Durham <TylerDurham@users.noreply.github.com>
2022-05-07 14:37:31 -0500

    Expose run() to shebang bootstrap.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import { isDockerInstalled } from "./docker";
 import { style } from "./style";
 
-if (!isDockerInstalled()) {
-    console.warn(`${style.bold(style.error('WARNING!'))} ${style.warning('Docker does not appear to be installed.')}`);
-} 
+const run = () => {
+    if (!isDockerInstalled()) {
+        console.warn(`${style.bold(style.error('WARNING!'))} ${style.warning('Docker does not appear to be installed.')}`);
+    }
+}
+
+export { run }
 
 
